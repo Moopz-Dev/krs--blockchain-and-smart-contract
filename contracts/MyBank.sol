97fabe910d179940548631fcd048f8754fbe0cd0
"Moopz_Dev" <"pongzthor@gmail.com">
2022-04-25 20:39:55 +0700

    feat: updates MyBank.sol

--- contracts/MyBank.sol
+++ contracts/MyBank.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+struct Manager{
+    string name;
+    uint age;
+    address account;
+
+}
 contract MyBank {
-    uint public amount;
-    function sendEther()payable public{
-        amount = msg.value;
+    uint _balance = 1000;
+    Manager public manager;
+    constructor(string memory _name,uint _age){
+        manager.name = _name;
+        manager.age = _age;
+        manager.account =  msg.sender;
     }
-    function getAddressContract() public view returns(address){
-        return address(this);
-    }
-    function getBalance()public view returns(uint){
-        return address(this).balance;
+    function getBalance() public view returns(uint) {
+        return _balance;
     }
 }
