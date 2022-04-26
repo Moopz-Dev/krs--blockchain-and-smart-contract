9cadf77f40f6c5a59bb7c3c334d6beb58c727735
"Moopz_Dev" <"pongzthor@gmail.com">
2022-04-26 20:30:13 +0700

    feat: adds enum and modifier

--- contracts/MyBank.sol
+++ contracts/MyBank.sol
@@ -7,14 +7,26 @@ struct Manager{
 
 }
 contract MyBank {
+    enum State {Open,Closed}
     uint _balance = 1000;
     Manager public manager;
+    State public bankState = State.Open; 
     constructor(string memory _name,uint _age){
         manager.name = _name;
         manager.age = _age;
         manager.account =  msg.sender;
     }
-    function getBalance() public view returns(uint) {
+
+    modifier isManager{
+        require(msg.sender == manager.account,"You don't have permission to use this function.");
+        _;    
+    }
+
+    function getBalance() public view isManager returns(uint) {
+        require(bankState == State.Open,"Service not available, bank is closed.");
         return _balance;
+    }
+    function getManager() public view isManager returns (string memory ){
+        return manager.name;
     }
 }
